fcc5ea68ffdc8ceb013624d4fda06569e11c65a3
Phhanuel <aureliennaas@gmail.com>
2025-07-15 14:43:33 +0200

    Add miner functionality and update display for ore and miners

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let wheat = 100; // Starting wheat amount
 let wood = 0; // Starting wood amount
+let ore = 0; // Starting ore amount
+
 let farmers = 0; // Starting farmers amount
 let lumberjacks = 0; // Starting lumberjacks amount
+let miners = 0; // Starting miners amount
 
 
 const farmerCost = 10; // Cost to buy a farmer
 const lumberjackCost = 15; // Cost to buy a lumberjack
+const minerCost = 15; // Cost to buy a miner
 
 //function harvestWheat() {
   //wheat++;
@@ -28,14 +32,28 @@ function buyLumberjacks() { // Function to buy a lumberjack
   }
 }
 
+function buyMiner() { // Function to buy a miner
+  if (wheat >= minerCost) { // Check if enough wheat to buy a miner
+    wheat -= minerCost;
+    miners++; // Increment the number of miners
+    updateDisplay();
+  }
+}
+
 function updateDisplay() { // Function to update the display
   document.getElementById("wheat-count").textContent = `You have ${wheat} wheat`;
   document.getElementById("farmer-count").textContent = `You have ${farmers} farmers`;
+
   document.getElementById("wood-count").textContent = `You have ${wood} wood`;
   document.getElementById("lumberjack-count").textContent = `You have ${lumberjacks} lumberjacks`;
 
+  document.getElementById("ore-count").textContent = `You have ${ore} wood`;
+  document.getElementById("miner-count").textContent = `You have ${miners} lumberjacks`;
+
+
   document.getElementById("buy-farmer-btn").disabled = wheat < farmerCost; // Disable button if not enough wheat
   document.getElementById("buy-lumberjack-btn").disabled = wheat < lumberjackCost; // Disable button if not enough wheat
+  document.getElementById("buy-miner-btn").disabled = wheat < minerCost; // Disable button if not enough wheat
 
 }
 
@@ -43,6 +61,7 @@ function updateDisplay() { // Function to update the display
 setInterval(() => { // Production automatique toutes les 1 secondes
   wheat += farmers;
   wood += lumberjacks;
+  ore += miners;
 
   updateDisplay();
 }, 1000);
