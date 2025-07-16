dd4473b6909c5a95f6918b0d6d1118d7936e805d
Phhanuel <aureliennaas@gmail.com>
2025-07-16 14:39:58 +0200

    Add blank line for improved readability in script.js

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ let warehouse = 0; // Starting warehouse amount
 let WoodWarehouseCost = 100;
 let OreWarehouseCost = 100;
 
+
 const farmerCost = 10; // Cost to buy a farmer
 const lumberjackCost = 15; // Cost to buy a lumberjack
 const minerCost = 15; // Cost to buy a miner
